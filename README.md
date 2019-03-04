b7ffccdd1ca4707915f95aa91667259596dbdc55
Zeev Rotshtein <zeev@vdoo.com>
2019-03-04 18:46:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Fold to Definitions README
+
+This extension allows you to fold all class/function/method definitions without folding the if/for/while/try/etc. blocks inside.
+
+## Features
+
+Just the description. Not much here yet. This is mainly for my own use.
+
+## Requirements
+
+None
+
+## Extension Settings
+
+None yet
+
+## Known Issues
+
+Relies on symbols provided by language extentions.
+
+## Release Notes
+
+First version. WOO!
+
+### 0.5.1
+
+Initial release
---
+++ src/extension.ts
@@ -0,0 +1,79 @@
+import * as vscode from 'vscode';
+
+let KINDS_TO_FOLD = [vscode.SymbolKind.Method, vscode.SymbolKind.Property, vscode.SymbolKind.Constructor, vscode.SymbolKind.Function];
+let KINDS_TO_FOLD_CLASSES = [vscode.SymbolKind.Class];
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('extension.fold_to_definitions', foldToDefinitions));
+	context.subscriptions.push(vscode.commands.registerCommand('extension.fold_to_class_definitions', foldToClassDefinitions));
+}
+
+export function deactivate() {}
+
+function foldToClassDefinitions() {
+	foldToDefinitions(true);
+}
+
+function foldToDefinitions(classes: boolean = false) {
+	const activeEditor = vscode.window.activeTextEditor;
+
+	if (activeEditor === undefined) {
+		return;
+	}
+
+	vscode.commands.executeCommand<vscode.DocumentSymbol[]>("vscode.executeDocumentSymbolProvider", activeEditor.document.uri).then(
+		function (symbols: vscode.DocumentSymbol[] | undefined) {
+			//console.log("symbols", symbols);
+
+			if (symbols === undefined) {
+				return;
+			}
+
+			let allSymbols: vscode.DocumentSymbol[] = [];
+			populateAllSymbols(symbols, allSymbols);
+
+			let kinds = KINDS_TO_FOLD;
+			if (classes) {
+				kinds = kinds.concat(KINDS_TO_FOLD_CLASSES);
+			}
+
+			allSymbols = allSymbols.filter(symbol => {
+				return kinds.includes(symbol.kind);
+			});
+
+			allSymbols.sort((a, b) => {
+				return b.range.start.line - a.range.start.line;
+			});
+
+			//console.log("allSymbols", symbols);
+
+			actuallyFold(activeEditor, allSymbols);
+		}
+	);
+}
+
+function populateAllSymbols(source: vscode.DocumentSymbol[], dest: vscode.DocumentSymbol[]) {
+	for (let symbol of source) {
+		dest.push(symbol);
+		if (symbol.children) {
+			populateAllSymbols(symbol.children, dest);
+		}
+	}
+}
+
+async function actuallyFold(activeEditor: vscode.TextEditor, symbols: vscode.DocumentSymbol[]) {
+	let original_selection = activeEditor.selection;
+
+	await vscode.commands.executeCommand("editor.unfoldAll");
+
+	for (let symbol of symbols) {
+		console.log("Folding", vscode.SymbolKind[symbol.kind], symbol.name, "in line", symbol.range.start.line);
+		activeEditor.selection = new vscode.Selection(symbol.selectionRange.start, symbol.selectionRange.start);
+		if (symbol.range.contains(original_selection)) {
+			original_selection = activeEditor.selection;
+		}
+		await vscode.commands.executeCommand("editor.fold");
+	}
+
+	activeEditor.selection = original_selection;
+}
